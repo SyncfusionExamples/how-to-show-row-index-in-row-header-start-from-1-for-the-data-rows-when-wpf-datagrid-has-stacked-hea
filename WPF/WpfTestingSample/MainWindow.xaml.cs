dc11f62ff1ae443b634f31fb6c3d568f7db148ed
Dhanasekar Mohanraj <dhanasekar.mohanraj@syncfusion.com>
2020-10-29 16:03:40 +0530

    How to show row index in row header start from 1 for the DataRows when the WPF DataGrid (SfDataGrid) has StackedHeaderRow and SummaryRow?

---
+++ WPF/WpfTestingSample/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfTestingSample.Model;
+
+
+namespace WpfTestingSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class RowIndexValueConverter : IValueConverter
+    {
+        SfDataGrid dataGrid = Application.Current.MainWindow.FindName("sfDataGrid") as SfDataGrid;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int firstRowIndex = dataGrid.GetFirstDataRowIndex();
+            int lastRowIndex = dataGrid.GetLastDataRowIndex();
+            if (value == null || (int)value > lastRowIndex || (int)value < firstRowIndex)
+                return string.Empty;
+            return (int)value - firstRowIndex + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF/WpfTestingSample/ViewModel/ViewModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfTestingSample.Model;
+
+namespace WpfTestingSample
+{
+    class ViewModel:INotifyPropertyChanged
+    {
+        EmployeeDetails emp = new EmployeeDetails();
+
+        private string rowHeaderText = "RowHeader";
+
+        public string RowHeaderText
+        {
+            get
+            {
+                return rowHeaderText;
+            }
+            set
+            {
+                rowHeaderText = value;
+                OnPropertyChanged("RowHeaderText");
+            }
+        }
+
+        #region Constructor
+
+        public ViewModel()
+        {
+            this.GDCSource = emp;
+        }
+
+        #endregion
+
+        private ObservableCollection<BusinessObjects> gdcsource;
+        public ObservableCollection<BusinessObjects> GDCSource
+        {
+            get
+            {
+                return gdcsource;
+            }
+            set
+            {
+                gdcsource = value;
+                OnPropertyChanged("GDCSource");
+            }
+        }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+
+    }
+
+    #region GDCSource DataSource
+    class EmployeeDetails : ObservableCollection<BusinessObjects>
+    {
+        Random rand = new Random();
+        public EmployeeDetails()
+        {
+            PopulateCollection();
+        }
+
+        private void PopulateCollection()
+        {
+            this.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                BusinessObjects b = new BusinessObjects() { EmployeeName = "Mart", EmployeeAge = 45, EmployeeArea = "U;SA", EmployeeSalary = 33000, ExperienceInMonth = 10, EmployeeGender = "Male",EmployeeDate=DateTime.Now };
+                this.Add(b);
+                b = new BusinessObjects() { EmployeeName = "Peter", EmployeeAge = 35, EmployeeArea = "UK", EmployeeSalary = 5678, ExperienceInMonth = 10, EmployeeGender = "Male", EmployeeDate = DateTime.Now };
+                this.Add(b);
+                b = new BusinessObjects() { EmployeeName = "James", EmployeeAge = 42, EmployeeArea = "UAE", EmployeeSalary = 18700, ExperienceInMonth = 10, EmployeeGender = "Male", EmployeeDate = DateTime.Now };
+                this.Add(b);
+                b = new BusinessObjects() { EmployeeName = "Oliver", EmployeeAge = 36, EmployeeArea = "USA", EmployeeSalary = 67000, ExperienceInMonth = 10, EmployeeGender = "Male", EmployeeDate = DateTime.Now };
+                this.Add(b);
+                b = new BusinessObjects() { EmployeeName = "Robert", EmployeeAge = 54, EmployeeArea = "India", EmployeeSalary = 34567, ExperienceInMonth = 10, EmployeeGender = "Male", EmployeeDate = DateTime.Now };
+                this.Add(b);
+                b = new BusinessObjects() { EmployeeName = "Suji", EmployeeAge = 45, EmployeeArea = "UK", EmployeeSalary = 90000, ExperienceInMonth = 10, EmployeeGender = "Female", EmployeeDate = DateTime.Now };
+                this.Add(b);
+                b = new BusinessObjects() { EmployeeName = "Mahesh", EmployeeAge = 48, EmployeeArea = "UK", EmployeeSalary = 34567, ExperienceInMonth = 10, EmployeeGender = "Male", EmployeeDate = DateTime.Now };
+                this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Ruby", EmployeeAge = 49, EmployeeArea = "UK", EmployeeSalary = 12345, ExperienceInMonth = 10, EmployeeGender = "Female" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Christain", EmployeeAge = 54, EmployeeArea = "India", EmployeeSalary = 80000, ExperienceInMonth = 10, EmployeeGender = "Male" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Aravind", EmployeeAge = 65, EmployeeArea = "India", EmployeeSalary = 12000, ExperienceInMonth = 10, EmployeeGender = "Male" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Daniel", EmployeeAge = 56, EmployeeArea = "USA", EmployeeSalary = 16000, ExperienceInMonth = 10, EmployeeGender = "Male" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Suhitha Azar", EmployeeAge = 78, EmployeeArea = "UK", EmployeeSalary = 98789, ExperienceInMonth = 10, EmployeeGender = "Female" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Praveen", EmployeeAge = 54, EmployeeArea = "UAE", EmployeeSalary = 45678, ExperienceInMonth = 10, EmployeeGender = "Male" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Stephen", EmployeeAge = 45, EmployeeArea = "USA", EmployeeSalary = 21000, ExperienceInMonth = 10, EmployeeGender = "Male" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Asha Joseph", EmployeeAge = 56, EmployeeArea = "India", EmployeeSalary = 56787, ExperienceInMonth = 10, EmployeeGender = "Female" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Clarke", EmployeeAge = 67, EmployeeArea = "UK", EmployeeSalary = 1200, ExperienceInMonth = 10, EmployeeGender = "Male" };
+                //this.Add(b);
+                //b = new BusinessObjects() { EmployeeName = "Dhileep Venkatesh", EmployeeAge = 45, EmployeeArea = "UK", EmployeeSalary = 45656, ExperienceInMonth = 10, EmployeeGender = "Male" };
+            }
+
+        }
+    }
+
+    #endregion
+}
